d6c9d7a048ff57d32a7c32849af099249e44c056
Basmala Ayman <basmala.ayman.1904@gmail.com>
2025-12-05 20:10:28 +0200

    fix insertApplicantProfile

--- src/main/java/com/jobsphere/dao/ApplicantDAO.java
+++ src/main/java/com/jobsphere/dao/ApplicantDAO.java
@@ -7,19 +7,21 @@ import java.sql.*;
 public class ApplicantDAO {
     //the applicant its skills is array of strings
 //we will use singleton here too to jsut dealing with just one object from it
-private static ApplicantDAO instance;
+    private static ApplicantDAO instance;
 
-private ApplicantDAO() {}
-
-public static ApplicantDAO getInstance() {
-    if (instance == null) {
-        instance = new ApplicantDAO();
+    private ApplicantDAO() {
     }
-    return instance;
-}
+
+    public static ApplicantDAO getInstance() {
+        if (instance == null) {
+            instance = new ApplicantDAO();
+        }
+        return instance;
+    }
+
     // Get applicant profile by its user ID
-    public Applicant getProfile(int userId) {
-        String sql = "SELECT * FROM applicant_profiles WHERE user_id = ?";
+    public Applicant getProfile(int userId) throws SQLException {
+        String sql = "SELECT * FROM applicants WHERE user_id = ?";
         Applicant applicant = null;
 
         try (Connection conn = DBConnection.getConnection();
@@ -30,25 +32,22 @@ public static ApplicantDAO getInstance() {
 
             if (rs.next()) {
                 applicant = new Applicant();
-                applicant.setUserId(rs.getInt("user_id"));
+                applicant.setId(rs.getInt("user_id"));
                 applicant.setPhone(rs.getString("phone"));
                 applicant.setResumeUrl(rs.getString("resume_url"));
                 applicant.setSkills(rs.getString("skills")); // JSON stored as string
                 applicant.setExperienceYears(rs.getInt("experience_years"));
             }
 
-        } catch (SQLException e) {
-            e.printStackTrace();
         }
-
         return applicant;
     }
 
     // Update applicant profile
     //this update function will update all the applicant information with the passed comming values
     //and it returned true or false indecate that update happened successfully or no
-    public boolean updateProfile(Applicant applicant) {
-        String sql = "UPDATE applicant_profiles SET phone = ?, resume_url = ?, skills = ?, experience_years = ? WHERE user_id = ?";
+    public boolean updateProfile(Applicant applicant) throws SQLException {
+        String sql = "UPDATE applicants SET phone = ?, resume_url = ?, skills = ?, experience_years = ? WHERE user_id = ?";
 
         try (Connection conn = DBConnection.getConnection();
              PreparedStatement stmt = conn.prepareStatement(sql)) {
@@ -57,23 +56,19 @@ public static ApplicantDAO getInstance() {
             stmt.setString(2, applicant.getResumeUrl());
             stmt.setString(3, applicant.getSkills());
             stmt.setInt(4, applicant.getExperienceYears());
-            stmt.setInt(5, applicant.getUserId());
+            stmt.setInt(5, applicant.getId());
 
             int rowsAffected = stmt.executeUpdate();
             return rowsAffected > 0;
 
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return false;
         }
     }
 
     // Insert a new applicant profile
-    public void insertApplicantProfile(Applicant applicant) {
-        String sql = "INSERT INTO applicant_profiles (user_id, phone, resume_url, skills, experience_years) " +
-                "VALUES (?, ?, ?, ?, ?)";
-        try (Connection conn = DBConnection.getConnection();
-             PreparedStatement stmt = conn.prepareStatement(sql)) {
+    public void insertApplicantProfile(Connection conn, Applicant applicant) throws SQLException {
+        String sql = "INSERT INTO applicants (user_id, phone, resume_url, skills, experience_years) " +
+                "VALUES (?, ?, ?, ?::json, ?)";
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
 
             stmt.setInt(1, applicant.getId());
             stmt.setString(2, applicant.getPhone());
@@ -83,8 +78,6 @@ public static ApplicantDAO getInstance() {
 
             stmt.executeUpdate();
 
-        } catch (SQLException e) {
-            e.printStackTrace();
         }
     }
 
